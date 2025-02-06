aa6c50ec4da893676049c3e740a120e32ebbfcca
Liams-theCreator <mmeellaallii1@gmail.com>
2025-02-06 18:51:11 +0100

    no changes

--- push_a2b.c
+++ push_a2b.c
@@ -6,7 +6,7 @@
 /*   By: imellali <imellali@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 16:34:22 by imellali          #+#    #+#             */
-/*   Updated: 2025/02/05 20:43:32 by imellali         ###   ########.fr       */
+/*   Updated: 2025/02/06 18:47:17 by imellali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@ static void	case_one(t_lista **stack_a, t_lista **stack_b, int position)
 {
 	int	i;
 
-	i = 1;
-	while (i <= position)
+	i = 0;
+	while (i < position)
 	{
 		rb(stack_b, 1);
 		i++;
 	}
 	pb(stack_a, stack_b);
-	i = 1;
-	while (i <= position)
+	i = 0;
+	while (i < position)
 	{
 		rrb(stack_b, 1);
 		i++;
@@ -35,25 +35,19 @@ static void	case_two(t_lista **stack_a, t_lista **stack_b, int position)
 {
 	int	i;
 
-	i = 1;
-	while (i <= position)
+	i = 0;
+	while (i < position)
 	{
 		rrb(stack_b, 1);
 		i++;
 	}
 	pb(stack_a, stack_b);
-	i = 1;
-	while (i <= position)
+	i = 0;
+	while (i < position)
 	{
 		rb(stack_b, 1);
 		i++;
 	}
-}
-
-static void	case_three(t_lista **stack_a, t_lista **stack_b)
-{
-	pb(stack_a, stack_b);
-	rb(stack_b, 1);
 }
 
 static void	exec_moves(t_lista **stack_a, t_lista **stack_b, int position, int length)
@@ -62,27 +56,23 @@ static void	exec_moves(t_lista **stack_a, t_lista **stack_b, int position, int l
 
 	len = length / 2;
 	if (position == 0)
+	{
 		pb(stack_a, stack_b);
-	else if (position == length)
-		case_three(stack_a, stack_b);
-	else if (position < len)
-		case_two(stack_a, stack_b, position);
-	else
+		rb(stack_b, 1);
+	}
+	else if (position <= len)
 		case_one(stack_a, stack_b, position);
+	else
+		case_two(stack_a, stack_b, position);
 }
 
 void    push_a2b(t_lista **stack_a, t_lista **stack_b)
 {
-    int len;
     int position;
 
-	len = list_len(*stack_a);
-	rb(stack_b, 1);
-	while (len > 3)
+	while (list_len(*stack_a) > 3)
 	{
 		position = find_position(*stack_b, (*stack_a)->num);
-		printf("pos : %d\n", position);
 		exec_moves(stack_a, stack_b, position, list_len(*stack_b));
-		len--;
 	}
 }
